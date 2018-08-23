1689b7373d7cc09d7166b5dacfc43079bc44426c
yuuuni1211 <yuu.uni1211@gmail.com>
2018-08-23 13:58:42 +0900

    add validates

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,2 +1,7 @@
 class Favorite < ApplicationRecord
+  validates :name, presence: true
+  validates :year, presence: true , numericality: {less_than: 2100}
+  validates :month, presence: true, numericality: {less_than: 12}
+  validates :day, presence: true, numericality: {less_than: 31}
+  validates :sex, presence: true
 end
