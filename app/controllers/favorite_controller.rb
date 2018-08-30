851fb1b2d2d955f4dee63673b3da25f103ae6f47
hashy0917 <oohasi3939+git@gmail.com>
2018-08-30 11:59:33 +0900

    Twitterシェア機能を実装しました リザルトページにシェアボタンがあります。 Twitterの仕様上、URLに`&`が使えないのでツイートから結果に飛ぶことはできません・・・

--- app/controllers/favorite_controller.rb
+++ app/controllers/favorite_controller.rb
@@ -1,18 +1,26 @@
 class FavoriteController < ApplicationController
-    before_action :authenticate_user!
-  
-    def index
-      @favorites = Favorite.where(user_id: current_user.id)
-    end
+  before_action :authenticate_user!
 
-    def edit
-      @favorites = Favorite.where(user_id: current_user.id)
-    end 
-
-    def destroy
-      @favorites = Favorite.where(user_id: current_user.id)
-
-    end
-    
+  def index
+    @favorites = Favorite.where(user_id: current_user.id)
   end
-     
+
+  def edit
+    @favorites = Favorite.where(user_id: current_user.id)
+  end 
+
+  def delete
+    @favorites = Favorite.find_by(params[:id])
+    @id = params[:id]
+
+    p "#{@favorites},(#{@id})"
+
+    @favorites.destroy
+
+    p "#{@favorites} ,(#{@id})Delete."
+
+    redirect_to action: :edit
+
+  end
+
+end
--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -15,6 +15,9 @@ class SearchesController < ApplicationController
     @day = params[:day]
     @sex = params[:sex]
     @favorite = Favorite.new
+
+    @URL = "result?day=#{@day}&month=#{@month}&sex=#{@sex}&year=#{@year}"
+
   end
 
   def diagnosis
@@ -30,17 +33,17 @@ class SearchesController < ApplicationController
 
     if [year,month,day,sex].all? { |item| item.present? } then
 
-    path = Rails.root.join('tmp', 'cache', "#{year}#{month}#{day}#{sex}.json")
+      path = Rails.root.join('tmp', 'cache', "#{year}#{month}#{day}#{sex}.json")
+      unless File.exists?(path) then
+        File.open(path, 'w+') { |f| f.write(json) }
+      end
 
-    unless File.exists?(path) then
-    File.open(path, 'w+') { |f| f.write(json) }
+      session[:result] = path
+      redirect_to action: :result, year: year, month: month, day: day, sex: sex
+    else
+      redirect_to root_path
+    end
   end
-    session[:result] = path
-    redirect_to action: :result, year: year, month: month, day: day, sex: sex
-  else
-    redirect_to root_path
-  end
-end
 
   def create
 
@@ -56,5 +59,5 @@ end
       else
         redirect_to action: :result
       end
-end
+  end
 end
