ee805d18cf82892ab3ef8d493ea615aa9adb611f
yuuuni1211 <yuu.uni1211@gmail.com>
2018-08-31 15:05:17 +0900

    fix/bag

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,6 +1,6 @@
 class Favorite < ApplicationRecord
   validates :name, presence: true
-  validates :year, presence: true , numericality: {less_than: 2100}
+  validates :year, presence: true , numericality: {greater_than_or_equal_to: 1900,less_than_or_equal_to:2100}
   validates :month, presence: true, numericality: {less_than: 13}
   validates :day, presence: true, numericality: {less_than: 32}
   validates :sex, presence: true
