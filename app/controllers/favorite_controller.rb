b6ff9c6ace638c9052b076706c740c3976ac741c
hibikiNPC <hibiki.fox@gmail.com>
2018-08-31 10:50:54 +0900

    マージ作業中に見つけた既知のバグを修正しました

--- app/controllers/favorite_controller.rb
+++ app/controllers/favorite_controller.rb
@@ -10,7 +10,7 @@ class FavoriteController < ApplicationController
   end 
 
   def delete
-    @favorites = Favorite.find_by(params[:id])
+    @favorites = Favorite.find(params[:id])
     @id = params[:id]
 
     p "#{@favorites},(#{@id})"
